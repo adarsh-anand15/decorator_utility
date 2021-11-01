8da66d3935913724fa87d9e4cd2bfe9ca3b13596
Adarsh Anand <adarsh.a@karza.in>
2021-11-01 16:27:29 +0530

    added github url and personal email

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     scripts=['decorator_utility.py'],
     description='Some useful decorators',
     version='1.0.0',
-    url='https://bitbucket.org/karzads/decorator_utility/',
+    url=['https://bitbucket.org/karzads/decorator_utility/', 'https://github.com/adarsh-anand15/decorator_utility'],
     author='Adarsh Anand',
-    author_email='adarsh.a@karza.in',
+    author_email=['adarsh.a@karza.in', 'adarsh.anand15@gmail.com'],
 )
